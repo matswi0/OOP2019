ccb5a082af17b24d1dc813604b980bbfc1b547c9
150514 <47736311+matswi0@users.noreply.github.com>
2019-10-25 14:30:12 +0200

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace w02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Blok 1");
+            blok b1 = new blok();
+            b1.iloscPieter = 10;
+            b1.senior = true;
+            b1.CzyWinda();
+            Console.WriteLine("Blok 2");
+            blok b2 = new blok();
+            b1.iloscPieter = 3;
+            b1.senior = true;
+            b2.CzyWinda();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ blok1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace w02
+{
+    class blok
+    {
+        public int iloscPieter;
+        public bool senior;
+        public void CzyWinda()
+        {
+            this.iloscPieter = iloscPieter;
+            this.senior = senior;
+            if (this.iloscPieter < 3 && this.senior == false)
+                Console.WriteLine("Winda nie jest potrzebna");
+            else if (this.iloscPieter >= 3 && this.senior == true)
+                Console.WriteLine("Winda jest potrzebna");
+        }
+    }
+}
